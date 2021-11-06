5a5e4264c560ecfc6fd8883390186013fdafef35
Harsohail Brar <harsohail.brar1@gmail.com>
2021-11-06 10:49:57 -0600

    attach login user to api

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,28 +2,40 @@ var express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const { SUCCESS, NOT_FOUND } = require("../utils/http");
+const { SUCCESS, NOT_FOUND, UNAUTHORIZED } = require("../utils/http");
 
 /* Check if a user exists */
 router.get("/user", async (req, res) => {
-    const { username, password } = req.query;
+  const { username, password } = req.query;
 
-    const foundUser = await User.findOne({ username: username, password: password });
+  const foundUser = await User.findOne({
+    username: username,
+    password: password
+  });
 
-    if (foundUser) {
-        res.status(SUCCESS).send("OK");
-    }
-    else {
-        res.status(NOT_FOUND).send("User not found");
-    }
-})
+  if (foundUser) {
+    delete foundUser.password; // TODO: password isnt deleted
+    res.send(foundUser);
+  } else {
+    res.status(NOT_FOUND).send("User not found");
+  }
+});
 
 /* Creating a user endpoint. */
 router.post("/user", async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    await new User({ username, password }).save();
-    res.status(SUCCESS).send("OK");
+  const foundUser = await User.findOne({
+    username: username
+  });
+
+  if (foundUser) {
+    res.status(UNAUTHORIZED).send("User already exists.");
+  } else {
+    const newUser = await new User({ username, password }).save();
+    delete newUser.password; // TODO: password isnt deleted
+    res.send(newUser);
+  }
 });
 
 module.exports = router;
