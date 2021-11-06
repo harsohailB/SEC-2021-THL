516d1bcb1b02883c46856a37f1cbf88b1c76b951
Harsohail Brar <harsohail.brar1@gmail.com>
2021-11-06 10:15:21 -0600

    add portoflio page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
-import React, { useEffect, useContext, Suspense, lazy } from "react";
+import React, { useEffect, useContext, Suspense } from "react";
 import { UserContext } from "./contexts/UserContext";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
-import ls from "local-storage";
 import Navbar from "./components/Navbar";
-import { LOGIN } from "./contexts/types";
+import ls from "local-storage";
 import { LinearProgress } from "@material-ui/core";
 import history from "./utils/history";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Watchlist from "./components/Watchlist";
+import Portfolio from "./components/Portfolio";
+import { LOGIN } from "./contexts/types";
 
 const App = () => {
+  const [user, dispatchToUser] = useContext(UserContext);
+
+  useEffect(() => {
+    // if nothing, check localstorage to see if user is saved
+    const userFromLocalStorage = ls.get("user");
+    if (userFromLocalStorage) {
+      dispatchToUser({ type: LOGIN, payload: userFromLocalStorage });
+    } else {
+      dispatchToUser({ type: LOGIN, payload: null });
+    }
+  }, [dispatchToUser]);
+
   return (
     <div className="px-8">
       <BrowserRouter history={history}>
@@ -20,6 +34,8 @@ const App = () => {
           <Switch>
             <Route path="/login" exact component={Login} />
             <Route path="/signup" exact component={SignUp} />
+            <Route path="/portfolio" exact component={Portfolio} />
+            <Route path="/watchlist" exact component={Watchlist} />
             <Route path="/" exact component={HomePage} />
           </Switch>
         </Suspense>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { LOGOUT } from "../contexts/types";
 
 const Navbar = () => {
+  const [user, dispatchToUser] = useContext(UserContext);
+  console.log("user", user);
+
+  const handleLogout = () => {
+    dispatchToUser({ type: LOGOUT, payload: user });
+  };
+
   return (
     <div className="flex-col flex md:flex-row items-center font-spartan pt-4 md:py-8 dark:text-white">
       <div className="flex justify-between items-center w-full mb-4 lg:mb-0">
-        <a className="font-extrabold" href="/">
-          Crypto Tracker
-        </a>
+        <div className="flex">
+          {" "}
+          <a className="font-extrabold" href="/">
+            Crypto Tracker
+          </a>
+          {user && <span className="ml-4">User: {user.username}</span>}
+        </div>
 
-        <div className="items-center flex">
-          <a className="mx-4 hidden md:flex" href="signup">
-            Sign Up
-          </a>
-          <a className="mx-4 hidden md:flex" href="login">
-            Login
-          </a>
+        <div className="items-center">
+          {!user ? (
+            <div className="flex">
+              <a className="mx-4 hidden md:flex" href="signup">
+                Sign Up
+              </a>
+              <a className="mx-4 hidden md:flex" href="login">
+                Login
+              </a>
+            </div>
+          ) : (
+            <div className="flex">
+              <a className="mx-4 hidden md:flex" href="portfolio">
+                Portfolio
+              </a>
+              <a className="mx-4 hidden md:flex" href="Watchlist">
+                Watchlist
+              </a>
+              <span className="mx-4 hidden md:flex" onClick={handleLogout}>
+                Logout
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
