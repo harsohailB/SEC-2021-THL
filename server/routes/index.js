f1ebab3d1f55e70eb2006be75989e4ad6ce9485f
Luke Son <flare2930@gmail.com>
2021-11-06 10:03:44 -0600

    Search/List APIs working

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,23 @@ const CoinGecko = require('coingecko-api');
 //2. Initiate the CoinGecko API Client
 const CoinGeckoClient = new CoinGecko();
 
-/* GET home page. */
+/* API to get list of names of coins */
+router.get("/coins/list", async function (req, res) {
+  let response = await CoinGeckoClient.coins.list();
+
+  //response's data contains array of objects that contain the name
+  res.send({ data: response.data });
+});
+
+/* Search API */
+router.get("/coins/search/:coinId", async function (req, res) {
+  // pass in user query into coinId sub URL, then fetch the coin that the user searched
+  let response = await CoinGeckoClient.coins.fetch(req.params.coinId, {});
+
+  //response's data contains array of objects that contain the name
+  res.send({ data: response.data });
+});
+
 router.get("/", async function (req, res) {
   let data = await CoinGeckoClient.ping();
   res.send({ test: data });
