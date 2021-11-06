f5d9ec37737863ac1acf1d650d9ad52d256160d7
Luke Son <flare2930@gmail.com>
2021-11-06 10:16:18 -0600

    Adding templates for watchlist and buy API

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,27 +11,40 @@ const CoinGeckoClient = new CoinGecko();
 router.get("/coins/list", async function (req, res) {
   let response = await CoinGeckoClient.coins.list();
 
-  //response's data contains array of objects that contain the name
+  // response's data contains array of objects that contain the names of all coins
   res.send({ data: response.data });
 });
 
-/* Search API */
+/* Search API 
+*  Once coin is retrieved from API, frontend will provide two options (and call appropriate API to accomplish each action)
+*  1. Adding coin to watchlist (Add to watchlist API)
+*  2. Buying coin (Buy coin API)
+*/
 router.get("/coins/search/:coinId", async function (req, res) {
   // pass in user query into coinId sub URL, then fetch the coin that the user searched
   let response = await CoinGeckoClient.coins.fetch(req.params.coinId, {});
 
-  //response's data contains array of objects that contain the name
   res.send({ data: response.data });
 });
 
-router.get("/", async function (req, res) {
-  let data = await CoinGeckoClient.ping();
-  res.send({ test: data });
+/* Add to watchlist API */
+router.get("/watchlist/add", async function (req, res) {
+  // adding coins to be tracked by user
 });
 
-router.get("/api", function (req, res, next) {
-  res.send({ test: "what" });
+/* Retrieve coins in watchlist API */
+router.get("/watchlist/list", async function (req, res) {
+
 });
 
+/* Buy coin API 
+*  API will be called from two possible locations:
+*  1. Click "buy" from searching coin
+*  2. Clicking "quick buy" from searching coin
+*/
+router.get("/coins/buy/:coinID/:quantity", async function (req, res) {
+  // params contains coinID and quantity
+  let params = req.params
+});
 
 module.exports = router;
